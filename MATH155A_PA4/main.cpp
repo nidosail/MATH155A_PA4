fbca423aa8f908bdc53b47ea13ef71c89903c352
Nicholas Dorn <nidosail@gmail.com>
2020-11-05 11:19:39 -0800

    Initial Commit

---
+++ MATH155A_PA4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MATH155A_PA4
+//
+//  Created by Nicholas Dorn on 11/5/20.
+//  Copyright © 2020 Nicholas Dorn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
